d2c8c81247c07a76f6c08fe69c7170024a89528e
Vu Anh Tuan <tuan.vu-anh@polytechnique.org>
2017-11-30 12:01:44 +0100

    Removed a few bugs.

--- table.py
+++ table.py
@@ -11,8 +11,8 @@ class Controller(object):
         self.lows = lows
         self.highs = highs
         self.n_bins = n_bins
-        self.action_space = range(n_actions)
-        self.Qmean = np.zeros((n_bins, n_bins, n_actions))
+        self.n_actions = n_actions
+        self.Qtable = np.zeros((n_bins, n_bins, n_actions))
         self.gamma = gamma
         self.epsilon = epsilon
 
@@ -21,32 +21,33 @@ class Controller(object):
         return np.array((state - self.lows) / steps, dtype=np.int)
 
     def optimal_action(self, state):
-        if np.random.uniform(0., 1.) < self.epsilon:
-            return np.random.choice(self.action_space)
+        if np.random.uniform(0, 1) < self.epsilon:
+            return np.random.choice(self.n_actions)
         else:
             ix, iy = self.discretize(state)
-            logits = np.exp(self.Qmean[ix, iy])
-            logits = logits / np.sum(logits)
-            return np.random.choice(self.action_space, p=logits)
+            logits = np.exp(self.Qtable[ix, iy])
+            logits /= np.sum(logits)
+            return np.random.choice(self.n_actions, p=logits)
 
     def train(self, state, action, reward, next_state, alpha):
         ix, iy = self.discretize(state)
         next_ix, next_iy = self.discretize(next_state)
-        update = reward + self.gamma * np.max(self.Qmean[next_ix, next_iy, action])
-        self.Qmean[ix, iy, action] += alpha * (update - self.Qmean[ix, iy, action])
+        update = reward + self.gamma * np.max(self.Qtable[next_ix, next_iy])
+        self.Qtable[ix, iy, action] += alpha * (update - self.Qtable[ix, iy, action])
 
 
 if __name__ == "__main__":
     env = gym.make("MountainCar-v0")
     env.seed(0)
-    np.random.seed(0)
+    # np.random.seed(0)
 
     controller = Controller(
         lows=env.observation_space.low,
         highs=env.observation_space.high,
-        n_bins=40, n_actions=env.action_space.n)
+        n_bins=20, n_actions=env.action_space.n)
 
     scores = deque(maxlen=100)
+    benchmark = -110.
     episodes = 20000
     initial_alpha = 1.0 # Learning rate
     min_alpha = 0.003
@@ -58,12 +59,15 @@ if __name__ == "__main__":
         done = False
         while not done:
             action = controller.optimal_action(state)
+            a, b = controller.discretize(state)
             next_state, reward, done, _ = env.step(action)
             rewards += reward
-            state = next_state
             controller.train(state, action, reward, next_state, alpha)
+            state = next_state
 
         scores.append(rewards)
 
-        if episode % 100 == 0:
-            print("episode {} rewards {} average score {}".format(episode + 1, rewards, np.mean(scores)))
+        print("episode {} rewards {} average score {}".format(episode, rewards, np.mean(scores)))
+
+        if np.mean(scores) >= benchmark:
+            break
